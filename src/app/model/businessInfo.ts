9e6fb0b78dca04be451e1418787a943a22072657
maurerogb <maurer@live.co.uk>
2025-12-01 20:48:05 +0100

    resolved merge conflict

--- src/app/model/businessInfo.ts
+++ src/app/model/businessInfo.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import { EmployeeComponent } from './../pages/protected/public-service/employee/employee.component';
->>>>>>> a80c031ac0218eb4e69d7487b722423ecad6a4a4
 export interface BusinessInfoRequest {
   businessName:         string,
   businessTypeId:       number,
@@ -15,30 +11,3 @@ export interface BusinessInfoRequest {
   atokaCode:            string,
   acceptTM:             boolean,
 }
-<<<<<<< HEAD
-=======
-
-export interface Employee {
-  occupantDetailId: number;
-  surname: string;
-  firstName: string;
-  middleName: string;
-  title: string;
-  gender: string;
-  dateOfBirth: string;
-  phoneNumber: string;
-  ocupation: string;
-  imageUrl: string;
-  emailAddress?: any;
-  placeOfWorkId: number;
-  contactPerson: string;
-  businessPhoneNo: string;
-  employmentStartDate: string;
-  businessId: number;
-  branchId: number;
-  isCurrentJob: boolean;
-  confirmationStatus: string;
-  approvedOn: string;
-  approvedBy: string;
-}
->>>>>>> a80c031ac0218eb4e69d7487b722423ecad6a4a4
